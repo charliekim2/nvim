2d83c7e561fc183cd38b44e81dcff5ec20527084
charliekim2 <charlie2@ualberta.ca>
2023-10-17 10:02:06 -0600

    fix bad indentation

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,27 +11,27 @@ return {
     local actions = require("telescope.actions")
 
     telescope.setup({
-	extensions = {
-	    fzf = {
-	      fuzzy = true,                    -- false will only do exact matching
-	      override_generic_sorter = true,  -- override the generic sorter
-	      override_file_sorter = true,     -- override the file sorter
-	      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-	    }
-	  },
-      defaults = {
-        path_display = { "truncate " },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          },
-        },
-      },
-    })
+			extensions = {
+				fzf = {
+					fuzzy = true,                    -- false will only do exact matching
+					override_generic_sorter = true,  -- override the generic sorter
+					override_file_sorter = true,     -- override the file sorter
+					case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+				}
+			},
+			defaults = {
+				path_display = { "truncate " },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+					},
+				},
+			},
+		})
 
-    telescope.load_extension("fzf")
+		telescope.load_extension("fzf")
 
     -- set keymaps
     local keymap = vim.keymap -- for conciseness
