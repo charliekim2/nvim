9ab6c10c0d892448ca03110ba61e11ee28e3a5a8
charliekim2 <charlie2@ualberta.ca>
2023-10-17 12:45:16 -0600

    added markdown preview

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,4 +1,15 @@
+local defaults = {noremap = true, silent=true}
+
 vim.opt.relativenumber = true
 vim.opt.number = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
+
+local indent = function(opts)
+	vim.opt.shiftwidth = tonumber(opts.fargs[1])
+	vim.opt.tabstop = tonumber(opts.fargs[1])
+end
+
+vim.api.nvim_create_user_command("Indent", indent, {nargs = 1})
+
+vim.keymap.set("n","<leader>mm",":MarkdownPreviewToggle<cr>",defaults)
