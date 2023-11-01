95db515a586f8edaed7b97f55bfe0e2774563b06
charliekim2 <charlie2@ualberta.ca>
2023-11-01 16:38:55 -0600

    stuff

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,4 +1,4 @@
-local defaults = {noremap = true, silent=true}
+local defaults = { noremap = true, silent = true }
 
 vim.opt.relativenumber = true
 vim.opt.number = true
@@ -10,7 +10,8 @@ local indent = function(opts)
 	vim.opt.tabstop = tonumber(opts.fargs[1])
 end
 
-vim.api.nvim_create_user_command("Indent", indent, {nargs = 1})
+vim.api.nvim_create_user_command("Indent", indent, { nargs = 1 })
 
-vim.keymap.set("n","<leader>mm",":MarkdownPreviewToggle<cr>",defaults)
+vim.keymap.set("n", "<leader>mm", ":MarkdownPreviewToggle<cr>", defaults)
 
+vim.keymap.set("n", "<leader>tt", ":TransparentToggle<cr>", defaults)
