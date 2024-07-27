9bfced3e1c115ad2f2208f581911cb4baaf89f0f
Charlie Kim <charlie2@ualberta.ca>
2024-07-27 17:09:12 -0600

    cowsay

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -5,18 +5,24 @@ return {
 	config = function()
 		local alpha = require("alpha")
 		local dashboard = require("alpha.themes.dashboard")
+		local cowsay = string.format(
+			[[
+	___________________________________
+/ It is %-30s\
+\ %-36s/
+	-----------------------------------
+				 \   ^__^ 
+					\  (oo)\_______
+						 (__)\       )\/\
+								 ||----w |
+								 ||     ||
+		]],
+			os.date("%A, %d %B %Y"),
+			"Go and write some code already!!"
+		)
 
 		-- Set header
-		dashboard.section.header.val = {
-			"                                                     ",
-			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-			"                                                     ",
-		}
+		dashboard.section.header.val = vim.split(cowsay, "\n")
 
 		-- Set menu
 		dashboard.section.buttons.val = {
@@ -24,7 +30,7 @@ return {
 			dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
 			dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
 			dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("SPC ll", "󰁯  > Open Lazy plugin manager", "<cmd>Lazy<CR>"),
+			dashboard.button("SPC zz", "󰁯  > Open Lazy plugin manager", "<cmd>Lazy<CR>"),
 			dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
 		}
 
