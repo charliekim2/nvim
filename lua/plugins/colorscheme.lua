287ce6140c891c6d1b810d36aa6a0d6e3d902b65
Charlie Kim <charlie2@ualberta.ca>
2024-07-27 22:33:06 -0600

    colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,43 +1,18 @@
 return {
 	{
-		"rebelot/kanagawa.nvim",
-		lazy = true,
-		event = "ColorSchemePre",
-	},
-	{
-		"folke/tokyonight.nvim",
+		"pineapplegiant/spaceduck",
 		priority = 1000,
 		config = function()
-			vim.cmd.colorscheme("tokyonight")
+			vim.cmd.colorscheme("spaceduck")
 		end,
 	},
 	{
-		"rmehri01/onenord.nvim",
-		lazy = true,
-		event = "ColorSchemePre",
-		-- config = function()
-		-- 	require("onenord").setup({
-		-- 		styles = {
-		-- 			comments = "italic",
-		-- 			strings = "NONE",
-		-- 			keywords = "italic",
-		-- 		},
-		-- 	})
-		-- 	vim.cmd.colorscheme("onenord")
-		-- end,
-	},
-	{
-		"ellisonleao/gruvbox.nvim",
+		"rafi/awesome-vim-colorschemes",
 		lazy = true,
 		event = "ColorSchemePre",
 	},
 	{
-		"nyoom-engineering/oxocarbon.nvim",
-		lazy = true,
-		event = "ColorSchemePre",
-	},
-	{
-		"catppuccin/nvim",
+		"zenbones-theme/zenbones.nvim",
 		lazy = true,
 		event = "ColorSchemePre",
 	},
