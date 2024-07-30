a0a24475972ea3ab1e80f369af21f5b186108042
Charlie Kim <charlie2@ualberta.ca>
2024-07-29 18:07:38 -0600

    disable copilot by default

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,5 +3,6 @@ return {
 	config = function()
 		vim.keymap.set("n", "<leader>ce", ":Copilot enable<cr>", { desc = "Enable Copilot" })
 		vim.keymap.set("n", "<leader>cd", ":Copilot disable<cr>", { desc = "Disable Copilot" })
+		vim.cmd(":Copilot disable")
 	end,
 }
